5918260a95458283f17a1ca3a953bdb858f525c0
Marwane <marwane24012007@gmail.com>
2026-05-06 14:40:10 +0200

    Test protection branche

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-function App() {
-  return (
-    <div>
-      <h1>Ma Todolist</h1>
-      <p>Vue principale - version 1</p>
-    </div>
-  );
-}
-
-export default App;
+git add .
+git commit -m "Test protection branche"
+git push origin main
